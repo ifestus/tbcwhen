529bc34543968c69c13956c762df78328b11bbdc
Jacob Campbell <ifestus@pm.me>
2020-12-29 14:57:55 -0600

    update redirect link because news'

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-redirect_route = 'https://us.forums.blizzard.com/en/wow/t/patch-1136-and-naxxramas-release-dates/702176'
+# redirect_route = 'https://us.forums.blizzard.com/en/wow/t/patch-1136-and-naxxramas-release-dates/702176'
+redirect_route = 'https://www.warcrafttavern.com/tbc/news/leak-tbc-classic-beta-in-feb-release-in-may/'
 
 @app.route("/")
 def landing_page():
